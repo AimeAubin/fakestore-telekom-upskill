ac2ceff6bca3bdf95d94919a0d86c9f75352f029
Irakoze Aimé Aubin <aubin.irakoze@amalitech.com>
2024-04-22 19:04:32 +0200

    products page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, ProductsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Product } from '../types/products-type';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  constructor(private apiService: ApiService) {}
+
+  getProducts = (url: string): Observable<Product[]> => {
+    return this.apiService.get(url, {
+      responseType: 'json',
+    });
+  };
+}
